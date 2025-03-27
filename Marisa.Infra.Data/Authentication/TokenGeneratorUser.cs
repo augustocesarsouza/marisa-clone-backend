dc517428d529c87a675233dda64e64b6dc1c1509
augustocesarsouza <augustocesarsantana90@gmail.com>
2025-03-27 14:45:41 -0400

    arrumando tokenGeneratorUser token

--- Marisa.Infra.Data/Authentication/TokenGeneratorUser.cs
+++ Marisa.Infra.Data/Authentication/TokenGeneratorUser.cs
@@ -40,7 +40,8 @@ namespace Marisa.Infra.Data.Authentication
                 new Claim("userID", userId)
             };
 
-            var keySecret = _configuration["Key:Jwt"];
+            //var keySecret = _configuration["Key:Jwt"];
+            var keySecret = Environment.GetEnvironmentVariable("KEY_JWT");
 
             if (string.IsNullOrEmpty(keySecret) || keySecret.Length < 16)
                 return InfoErrors.Fail(new TokenOutValue(), "error token related");
