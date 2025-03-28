8c4da3bc8ecbb191a05179b50ad9510ed2d0d729
augustocesarsouza <augustocesarsantana90@gmail.com>
2025-03-28 07:35:56 -0400

    criei migration para trocar nome da table user

---
+++ Marisa.Infra.Data/Migrations/20250328112149_RenameTbUsersToTbMarisaUsers.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Marisa.Infra.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameTbUsersToTbMarisaUsers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(
+                name: "tb_users",
+                newName: "tb_marisa_users");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(
+                name: "tb_marisa_users",
+                newName: "tb_users");
+        }
+    }
+}
