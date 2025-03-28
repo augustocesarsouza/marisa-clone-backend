12250a17cf2e714bf6669256d8907ccb159854ce
augustocesarsouza <augustocesarsantana90@gmail.com>
2025-03-28 12:27:35 -0400

    test

--- Marisa.Infra.IoC/DataHelper.cs
+++ Marisa.Infra.IoC/DataHelper.cs
@@ -1,18 +1,35 @@
 ﻿using Marisa.Infra.Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace Marisa.Infra.IoC
 {
-    public static class DataHelper
+    public class DataHelper
     {
         public static async Task ManageDataAsync(IServiceProvider svcProvider)
         {
-            //Service: An instance of db context
+            // Obter o DbContext
             var dbContextSvc = svcProvider.GetRequiredService<ApplicationDbContext>();
 
-            //Migration: This is the programmatic equivalent to Update-Database
-            await dbContextSvc.Database.MigrateAsync();
+            // Obter o logger para registrar informações
+            var logger = svcProvider.GetRequiredService<ILogger<DataHelper>>();
+
+            // Verificar se há migrações pendentes
+            var pendingMigrations = await dbContextSvc.Database.GetPendingMigrationsAsync();
+
+            if (pendingMigrations.Any())
+            {
+                // Se houver migrações pendentes, aplicar as migrações
+                logger.LogInformation($"Há {pendingMigrations.Count()} migrações pendentes. Aplicando migrações...");
+                await dbContextSvc.Database.MigrateAsync();
+                logger.LogInformation("Migrações aplicadas com sucesso.");
+            }
+            else
+            {
+                // Caso não haja migrações pendentes
+                logger.LogInformation("Não há migrações pendentes.");
+            }
         }
     }
 }
