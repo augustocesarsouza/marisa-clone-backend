df8e3e19426949f5ca5ba4b2665209a2775b1e5e
augustocesarsouza <augustocesarsantana90@gmail.com>
2025-05-21 09:35:58 -0400

    alteração

---
+++ Marisa.Infra.Data/Migrations/20250521133209_AddressChangeNameTable.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Marisa.Infra.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddressChangeNameTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(
+                name: "tb_address",
+                newName: "tb_marisa_address");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_tb_address_user_id",
+                table: "tb_marisa_address",
+                newName: "IX_tb_marisa_address_user_id");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "type",
+                table: "tb_marisa_products",
+                type: "varchar(150)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "nvarchar(150)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(
+                name: "tb_marisa_address",
+                newName: "tb_address");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_tb_marisa_address_user_id",
+                table: "tb_address",
+                newName: "IX_tb_address_user_id");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "type",
+                table: "tb_marisa_products",
+                type: "nvarchar(150)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(150)");
+        }
+    }
+}
